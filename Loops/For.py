dad615c7af4e50c510217285e22d3281264eff49
ham70 <145173497+ham70@users.noreply.github.com>
2023-09-30 04:03:40 +0000

    for loop practice exercise completed

--- Loops/For.py
+++ Loops/For.py
@@ -7,5 +7,13 @@ for i in range(10):
 for i in range(4):
     print(i)
 
+#for loops exercise
+#print every number inbetween 10 and 30 that is divisable by 8
+for i in range(10, 30):
+    if i % 8 == 0:
+        print(i)
 
+#print all numbers from 0 to 10 raised tp the power of 1
+for i in range(11):
+    print(str(pow(i, 2)))
 
